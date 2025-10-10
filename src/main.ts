d41f446112f11293c373eafe2a70bc8e30a22316
Juanes OpenFintek <ds.ai@openfintek.co>
2025-10-09 22:36:09 -0500

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -55,8 +55,7 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
 
-  await app.listen(3001).then(() => {
-    console.log('Server running on port http://localhost:3001');
-  });
+  const port = Number(process.env.PORT) || 3001;
+  await app.listen(port, '0.0.0.0');
 }
 bootstrap();
